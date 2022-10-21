f3cde3c29ac8d8f8819bb945f7d7ffd162120c74
IvieMeireles <iviemeireles@gmail.com>
2022-10-20 23:49:24 -0300

    Exercício 8
    resolução do exercício 8

---
+++ Exercicio 8/app.js
@@ -0,0 +1,21 @@
+let qtd = 0;
+
+// 1º numero primo é 2, irá encerrar a contagem em 1000
+for (let n=2; n <= 10000; n++) { 
+    let primo = true;
+    for (let i = 2; i < n; i++) // contagem se inicia em 2, irá até o 1000 e irá parar no último antes de 1000
+    
+    if (n % i === 0){ // se o resto da divisão for exatamente 0, não é um número primo
+        primo = false;
+    }
+
+
+if (primo) {  //se o número for primo, irá somar a quantidade de números
+    qtd++;
+    console.log(n); // irá retornar TODOS os números primos
+ }
+}
+
+
+
+console.log(`De 0 a 10000 existem ${qtd} números primos`);
