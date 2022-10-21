a28671a511f17b6ca59f48a6f87f8251a3168bbd
IvieMeireles <iviemeireles@gmail.com>
2022-10-20 23:50:17 -0300

    atualização exercício 8
    Estava calculando até 10000, alterado para 1000

--- Exercicio 8/app.js
+++ Exercicio 8/app.js
@@ -1,7 +1,7 @@
 let qtd = 0;
 
 // 1º numero primo é 2, irá encerrar a contagem em 1000
-for (let n=2; n <= 10000; n++) { 
+for (let n=2; n <= 1000; n++) { 
     let primo = true;
     for (let i = 2; i < n; i++) // contagem se inicia em 2, irá até o 1000 e irá parar no último antes de 1000
     
@@ -18,4 +18,4 @@ if (primo) {  //se o número for primo, irá somar a quantidade de números
 
 
 
-console.log(`De 0 a 10000 existem ${qtd} números primos`);
+console.log(`De 0 a 1000 existem ${qtd} números primos`);
